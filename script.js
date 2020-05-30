9f4040ef5a36394b2842ef2378829a0372cde93b
Aykut Saki <aykutsakisocial@gmail.com>
2020-05-30 16:37:54 +0300

    Sign-in sign-up

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("signin-button").addEventListener("click", function () {
+    document.getElementById("sign-in").style.display = "flex";
+})
+
+document.getElementById("signup-button").addEventListener("click", function () {
+    document.getElementById("sign-up").style.display = "flex";
+})
+
+document.getElementsByClassName("close")[0].addEventListener("click", function () {
+    document.getElementById("sign-in").style.display = "none";
+})
+
+document.getElementsByClassName("close")[1].addEventListener("click",function () {
+    document.getElementById("sign-up").style.display = "none";
+})
+
+window.onclick = function (event) {
+    if (event.target == document.getElementById("sign-in")) {
+        document.getElementById("sign-in").style.display = "none";
+        return;
+    }
+    if (event.target == document.getElementById("sign-up")) {
+        document.getElementById("sign-up").style.display = "none";
+        return;
+    }
+
+}
+
